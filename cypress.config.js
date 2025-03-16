9eb9c4a7ddb59d66f64817926745099c75dd6df0
Tamas Juhasz <juhasztamas0705@gmail.com>
2025-03-16 16:00:22 +0100

    basic cucumber test runs

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,35 @@
-const { defineConfig } = require("cypress");
+const cucumber = require('cypress-cucumber-preprocessor').default
+const { defineConfig } = require("cypress")
+
 
 module.exports = defineConfig({
   e2e: {
-    fixturesFolder: "source/fixtures",
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber())
+
+      return config
+    },
+
+    cucumberJson: {
+      generate: true,
+      outputFolder: "source/cypress/reports/cucumber-json"
+    },
+
+    fixturesFolder: "source/cypress/fixtures",
     supportFile: false,
-    specPattern: "source/**/*.cy.js",
+    stepDefinitions: "source/cypress/support/step_definitions/",
+    baseUrl: 'https://magento.softwaretestingboard.com',
+    specPattern: "source/cypress/e2e/**/*.feature",
     chromeWebSecurity: false,
-    baseUrl: "https://magento.softwaretestingboard.com",
     screenshotsFolder: "source/cypress/screenshots",
     videosFolder: "source/cypress/videos", 
   },
-});
+  "cypress-cucumber-preprocessor": {
+    "nonGlobalStepDefinitions": false,
+    "json": {
+      "enabled": true,
+      "formatter": "source/support/cucumber-json-formatter",
+      "output": "source/cypress/reports/report.json"
+   }
+  }
+})
